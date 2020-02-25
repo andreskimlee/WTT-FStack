6e6004d6dbfe0186ca6db7bd4ad70701aebd6b72
Andres Kim <andreskimlee@gmail.com>
2020-02-25 12:55:26 -0500

    Debug deployment

---
+++ frontend/build/precache-manifest.b07b81cea1ee2dc8222cc67cd9e1d398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdbf7350e85581ef0053f39c96b6ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d20e852dbe2ca10ac6",
+    "url": "/static/css/2.11b8b2f7.chunk.css"
+  },
+  {
+    "revision": "1028533aff65076b3b05",
+    "url": "/static/css/main.ef92ef83.chunk.css"
+  },
+  {
+    "revision": "16d20e852dbe2ca10ac6",
+    "url": "/static/js/2.cb0b41e9.chunk.js"
+  },
+  {
+    "revision": "eaaf359e6284af6bdda1d61285291759",
+    "url": "/static/js/2.cb0b41e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1028533aff65076b3b05",
+    "url": "/static/js/main.856a0cd9.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "2e94add9b3f265eb3c8eaf0a0be46043",
+    "url": "/static/media/signin_wallpaper.2e94add9.png"
+  }
+]);
